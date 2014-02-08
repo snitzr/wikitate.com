c0a9a4571ebd2031f7a3841b28dca0d99e28a065
Aaron Snitzer <aaron.snitzer@gmail.com>
2014-02-08 01:28:05 -0500

    models refactor

--- dproject/admin.py
+++ dproject/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from dproject.models import Vid
-
-admin.site.register(Vid)
+from dproject.models import Vid, VidId,  Transcript, User, Language
 
 # Register your models here.
+admin.site.register(Vid)
+admin.site.register(VidId)
+admin.site.register(Transcript)
+admin.site.register(User)
+admin.site.register(Language)
--- dproject/models.py
+++ dproject/models.py
@@ -2,22 +2,37 @@ from django.db import models
 
 class Vid(models.Model):
     vidSource = models.CharField(max_length=400)
+
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.vidSource
+
+
+class VidId(models.Model):
     vidId = models.CharField(max_length=400)
 
     def __unicode__(self):  # Python 3: def __str__(self):
-        return self.question
+        return self.vidId
+
 
 class Transcript(models.Model):
     vid = models.ForeignKey(Vid)
-    user = models.CharField(max_length=50)
     transcript = models.CharField(max_length=50000000)
+
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.transcript
+
+
+class User(models.Model):
+    vid = models.ForeignKey(Vid)
+    user = models.CharField(max_length=50)
+
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.user
+
+
+class Language(models.Model):
+    vid = models.ForeignKey(Transcript)
     language = models.CharField(max_length=400)
 
     def __unicode__(self):  # Python 3: def __str__(self):
-        return self.question
-
-# class User(models.Model):
-    # user = models.CharField(max_length=200)
-
-    # def __unicode__(self):  # Python 3: def __str__(self):
-        # return self.question
+        return self.language 
