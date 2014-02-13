768c5f0dc07e1f3fe7c57daa32ff80965d285995
Aaron Snitzer <aaron.snitzer@gmail.com>
2014-02-13 11:39:42 -0500

    add unicode to model

--- dproject/models.py
+++ dproject/models.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 from django.db import models
 
 class Vid(models.Model):
@@ -32,7 +34,13 @@ class User(models.Model):
 
 class Language(models.Model):
     vid = models.ForeignKey(Transcript)
-    language = models.CharField(max_length=400)
+    LANGUAGES = (
+            ('fr', 'French'),
+            ('uk', 'Українська'),
+    )
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return u'%s' % (self.LANGUAGES)
+    language = models.CharField(max_length=2, choices=LANGUAGES)
 
     def __unicode__(self):  # Python 3: def __str__(self):
-        return self.language 
+        return u'%s' % (self.language)
