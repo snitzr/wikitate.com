eb1050b1b8acdb401d5a2f75a2286a2d7548a018
Aaron Snitzer <aaron.snitzer@gmail.com>
2014-02-18 19:47:25 -0500

    about page

--- dproject/urls.py
+++ dproject/urls.py
@@ -3,5 +3,6 @@ from dproject import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
     url(r'^(?P<vidId>.+)/$', views.indexvids, name='indexvids'),
 )
--- dproject/views.py
+++ dproject/views.py
@@ -5,5 +5,8 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'dproject/index.html')
 
+def about(request):
+    return render(request, 'dproject/about.html')
+
 def indexvids(request, vidId):
     return render(request, 'dproject/sub.html', {'vidId': vidId})
