bad15680a4a127bbb5472c65a12a294869a90608
Aaron Snitzer <aaron.snitzer@gmail.com>
2014-03-09 15:52:26 -0400

    comment formatting

--- dproject/views.py
+++ dproject/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, Http404, get_object_or_404, redirect
 from django.http import HttpResponse
 from dproject.forms import SearchForm
-#users
+# users
 from django.contrib.auth import authenticate, login
 
 # universal vid search bar
