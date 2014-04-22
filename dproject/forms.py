71ded61640a1bad3130d7545b0c514213f6d0221
Aaron Snitzer <aaron.snitzer@gmail.com>
2014-04-22 00:46:21 -0400

    form formatting

--- dproject/forms.py
+++ dproject/forms.py
@@ -6,14 +6,14 @@ from dproject import models
 class SearchForm(forms.Form):
     # dup of models
     # https://docs.djangoproject.com/en/1.6/topics/forms/modelforms/
-    Transcript = forms.CharField(max_length=50,
+    transcript = forms.CharField(max_length=50,
                                  label = '',
                                  widget=forms.TextInput(attrs={'placeholder': 'enter video link'}))
 
 class FormTest(forms.Form):
-    Transcript = forms.CharField(widget=forms.Textarea(attrs={'placeholder':
+    transcript = forms.CharField(widget=forms.Textarea(attrs={'placeholder':
                                                               'Add transcript'}))
 
 class LanguageModelChoiceField(forms.Form):
-    Language = forms.ChoiceField(choices = models.Transcript.LANGUAGES,
-                                 initial = {'asdf'})
+    language = forms.ChoiceField(choices = models.Transcript.LANGUAGES,
+                                 initial = {'blank'})
