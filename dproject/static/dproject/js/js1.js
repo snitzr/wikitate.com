a9c5a41b6c96e05a33c113fcf4c26c8b3b7ede5e
Aaron Snitzer <aaron.snitzer@gmail.com>
2014-06-28 16:29:30 -0400

    more log testing

--- dproject/static/dproject/js/js1.js
+++ dproject/static/dproject/js/js1.js
@@ -82,23 +82,30 @@ $('#timeClick').mouseup(function () {
 // append new time and text line to add transcript table on tab
 // allow tab through to Submit button if last text field is blank
 // make this function a variable to call? Currently having global vs local var issue because declaring var in function?
-$(this).focusin(function() {
+$('#transcripting').focusin(function() {
   // need logic to only append on last row
   // need logic to stop appending once new blank row is appended
   // jQuery logic to see if next tag is not table so to only append last row
   // tab order needs to be respected
   // add overflow feature in CSS
-  console.log('1 working');
-  console.log('2 activeElement value:\t' + (document.activeElement).value);
-  console.log('3 activeElement type:\t' + (document.activeElement).type);
+  console.log('\n\n_________________v\nworking');
+  console.log('1a activeElement value:\t' + document.activeElement.value);
+  console.log('1b activeElement type:\t' + document.activeElement.type);
   // test if previous cell has a value 
-  console.log('4 #runout_line prev val:\t' + $("#runout_line").prev().val());
+  console.log('2a this prev prev val:\t' + $(this).prev().prev().val());
+  console.log('2b this prev val:\t' + $(this).prev().val());
+  console.log('2c this prev:\t' + $(this).prev());
+  console.log('2d this siblings:\t' + $(this).siblings());
+  console.log('2e this next val:\t' + $(this).next().val()); // to check if next item is Submit button
+  console.log('3 #runout_line prev prev val:\t' + $('#runout_line').prev().prev().val());
+  console.log('4 #f3 prev prev val:\t' + $('#f3').prev().prev().val());
+  console.log('5 this parent parent prev children val:\t' + $(this).parent().parent().prev().children().children().val());
   // var prev_val = ($("#runout_line").prev().val());
   // var runout = (': #runout_line next val');
   // if runout === '' {
     // console.log('null next sibling');
   // }
-  if ((((document.activeElement).type) === 'text') && ((document.activeElement).value) !== '') {
+  if ((document.activeElement.type === 'text') && (document.activeElement.value !== '')) {
     // for maintainability the appended HTML should just make a copy of current tr minus text input and not be hardcoded HTML
     $('#transcripting').append('<tr id="runout_row"><td><input class="time" cols="100" min="0" type="number" placeholder="time" autocomplete="off" step="0.5"></td><td id="runout_line"><input class="transcript_cell" maxlength="100" placeholder="text" type="text" autocomplete="off"></td></tr>');
   }
