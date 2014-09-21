e5d7511356bd8aca2b672d3ff838278531ec4e7e
vagrant <vagrant@precise32.(none)>
2014-09-21 03:42:47 +0000

    databases redo

--- mysite/settings.py
+++ mysite/settings.py
@@ -166,7 +166,12 @@ ACCOUNT_USERNAME_BLACKLIST = ['root',
 # Parse database configuration from $DATABASE_URL
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+# DATABASES['default'] =  dj_database_url.config() #original
+
+# aaron mod
+DATABASES = {
+	'default': dj_database_url.config()
+}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
