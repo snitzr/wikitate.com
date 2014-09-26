336b22ee2a9bc16a618b6de4e23079dfc3f9498e
Aaron Snitzer <aaron.snitzer@gmail.com>
2014-09-26 13:18:43 -0400

    var declaration test

--- mysite/settings.py
+++ mysite/settings.py
@@ -162,6 +162,7 @@ ACCOUNT_USERNAME_BLACKLIST = ['root',
 # Parse database configuration from $DATABASE_URL
 
 import dj_database_url
+DATABASES = {}
 DATABASES['default'] =  dj_database_url.config() # aaron comment out
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
