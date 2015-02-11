0703d0a24fee3267fbc8c338de51dcf3df47a0b5
Aaron Snitzer <aaron.snitzer@gmail.com>
2015-02-11 01:32:08 -0500

    h2

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,3 +1,5 @@
+# HEROKU PRODUCTION
+
 """
 WSGI config for mystite project.
 
@@ -10,14 +12,14 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os, sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-# """
+"""
 # comment out so this original is not dup of Heroku
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-# """
-
-
 """
+
+
+# """
 #### begin heroku setup
 from django.core.wsgi import get_wsgi_application
 
@@ -26,4 +28,4 @@ from dj_static import Cling
 application = Cling(get_wsgi_application())
 
 #### end heroku setup
-"""
+# """
