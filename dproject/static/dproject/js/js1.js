72398b9bac0eb4031a057ef12e591cb732038f46
Aaron Snitzer <aaron@snitzer.me>
2015-09-15 19:06:17 -0400

    IE testing new interval 2

--- dproject/static/dproject/js/js1.js
+++ dproject/static/dproject/js/js1.js
@@ -54,6 +54,7 @@ function updateCurrentTime() {
     $('.timestamp_display').html('<a href="#notlink">' + '&nbsp;&nbsp;&nbsp;&nbsp;' + Math.round(rounded) + '&nbsp;&nbsp;&nbsp;&rArr;</a>');
     captionTime(rounded);
     console.log(rounded); // IE 9 testing.
+    console.log('updateCurrentTime'); // IE 9 testing.
   }
 }
 
